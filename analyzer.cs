788d464f9f26788fabf8e6fb0e149106e05eece5
Vlad Brezae <brezaevlad@gmail.com>
2015-09-02 16:58:18 -0700

    Do multiple runs and aggregate the stats

--- analyzer.cs
+++ analyzer.cs
@@ -45,6 +45,8 @@ public class Program {
 	public const string binprotFile = "/tmp/binprot";
 	public const string binprotExec = "sgen-grep-binprot";
 
+	public const int numRuns = 5;
+
 	public const int deltaHackConc = 4;
 	public const int deltaHackNoconc = deltaHackConc;
 
@@ -54,6 +56,9 @@ public class Program {
 	private static List<Interval> nurseryIntervals, majorIntervals, concurrentIntervals, stopIntervals;
 	private static PlotModel plotModel;
 	private static float referenceTime;
+
+	private static List<RunStats> noconcRunStats = new List<RunStats> ();
+	private static List<RunStats> concRunStats = new List<RunStats> ();
 
 	public static void Main (string[] args) {
 		if (args.Length < 2) {
@@ -65,41 +70,84 @@ public class Program {
 		string workingDirectory = args [1];
 		string target = Path.GetFileNameWithoutExtension (args[2]);
 		string resultsFolder = Path.Combine ("results", target);
-		string statsFile = Path.Combine (resultsFolder, "stats");
-		string svgFile = Path.Combine (resultsFolder, target + ".svg");
 
 		Directory.CreateDirectory (resultsFolder);
-		using (StreamWriter statsWriter = new StreamWriter (statsFile)) {
+		/* Reduce jit compilation delays */
+		Console.WriteLine (DateTime.Now.AddMilliseconds (100));
+		for (int i = 0; i < numRuns; i++) {
+			string svgFile = Path.Combine (resultsFolder, target + i + ".svg");
 			plotModel  = new PlotModel { Title = Path.GetFileNameWithoutExtension (args [1]) };
 			plotModel.Axes.Add (new LinearAxis { Position = AxisPosition.Bottom });
 			plotModel.Axes.Add (new LinearAxis { Position = AxisPosition.Left });
 
-			/* Reduce jit compilation delays */
-			Console.WriteLine (DateTime.Now.AddMilliseconds (100));
-
 			RunMono (mono, args.SubArray<string> (2), workingDirectory, false);
 			referenceTime = memoryUsage [memoryUsage.Count - 1].x;
-			ParseBinProtOutput (false);
+			ParseBinProtOutput ();
 			AddPlotData ();
-			OutputStats ("noconc", statsWriter);
+			noconcRunStats.Add (GetStats ());
 
 			RunMono (mono, args.SubArray<string> (2), workingDirectory, true);
-			ParseBinProtOutput (true);
+			ParseBinProtOutput ();
 			AddPlotData ();
-			OutputStats ("conc", statsWriter);
+			concRunStats.Add (GetStats ());
 
 			Plot (svgFile);
 		}
+
+		string statsFile = Path.Combine (resultsFolder, "stats");
+		using (StreamWriter statsWriter = new StreamWriter (statsFile)) {
+			OutputStat ("noconc", AggregateStats (noconcRunStats), statsWriter);
+			OutputStat ("conc", AggregateStats (concRunStats), statsWriter);
+		}
 	}
 
-	public static void OutputStats (string prefix, StreamWriter statsWriter) {
-		float totalTime = memoryUsage [memoryUsage.Count - 1].x;
-		statsWriter.WriteLine ("\n" + prefix);
-		/* Number of collections */
-		statsWriter.WriteLine ("Time (s)		{0}", totalTime);
-		statsWriter.WriteLine ("Minor			{0}", nurseryIntervals.Count);
-		statsWriter.WriteLine ("Major			{0}", majorIntervals.Count);
-		statsWriter.WriteLine ("Minor While Major	{0}", num_minor_while_major);
+	public static RunStats AggregateStats (List<RunStats> list)
+	{
+		RunStats aggregated = new RunStats ();
+		list.Sort ();
+
+		for (int i = 1; i < numRuns - 1; i++) {
+			RunStats current = list [i];
+
+			aggregated.totalTime += current.totalTime;
+			aggregated.minMinor = Math.Min (aggregated.minMinor, current.minMinor);
+			aggregated.maxMinor = Math.Max (aggregated.maxMinor, current.maxMinor);
+			aggregated.minMajor = Math.Min (aggregated.minMajor, current.minMajor);
+			aggregated.maxMajor = Math.Max (aggregated.maxMajor, current.maxMajor);
+			aggregated.avgMinor += current.avgMinor;
+			aggregated.avgMajor += current.avgMajor;
+			aggregated.totalPause += current.totalPause;
+			aggregated.numMinor += current.numMinor;
+			aggregated.numMinorStops += current.numMinorStops;
+			aggregated.numMajor += current.numMajor;
+			aggregated.numMajorStops += current.numMajorStops;
+			aggregated.numMinorWhileMajor += current.numMinorWhileMajor;
+			aggregated.memoryUsage += current.memoryUsage;
+		}
+
+		aggregated.totalTime /= numRuns - 2;
+		aggregated.avgMinor /= numRuns - 2;
+		aggregated.avgMajor /= numRuns - 2;
+		aggregated.totalPause /= numRuns - 2;
+		aggregated.numMinor /= numRuns - 2;
+		aggregated.numMinorStops /= numRuns - 2;
+		aggregated.numMajor /= numRuns - 2;
+		aggregated.numMajorStops /= numRuns - 2;
+		aggregated.numMinorWhileMajor /= numRuns - 2;
+		aggregated.memoryUsage /= numRuns - 2;
+
+		return aggregated;
+	}
+
+	public static RunStats GetStats ()
+	{
+		RunStats runStats = new RunStats ();
+
+		runStats.totalTime = memoryUsage [memoryUsage.Count - 1].x;
+		runStats.numMinor = nurseryIntervals.Count;
+		runStats.numMajor = majorIntervals.Count;
+		runStats.numMinorWhileMajor = num_minor_while_major;
+
 		/* Pause times */
 		float minMinor = float.MaxValue, maxMinor = float.MinValue, minMajor = float.MaxValue, maxMajor = float.MinValue, avgMinor = 0.0f, avgMajor = 0.0f, totalPause = 0.0f;
 		int numMinor = 0, numMajor = 0;
@@ -128,24 +176,45 @@ public class Program {
 		avgMinor /= numMinor;
 		avgMajor /= numMajor;
 
-		statsWriter.WriteLine ();
-		statsWriter.WriteLine ("Total pause time (ms)	{0}", totalPause * 1000);
-		statsWriter.WriteLine ("Minor stops		{0}", numMinor);
-		statsWriter.WriteLine ("Min minor (ms)		{0}", minMinor * 1000);
-		statsWriter.WriteLine ("Max minor (ms)		{0}", maxMinor * 1000);
-		statsWriter.WriteLine ("Avg minor (ms)		{0}", avgMinor * 1000);
-		statsWriter.WriteLine ("Major stops		{0}", numMajor);
-		statsWriter.WriteLine ("Min major (ms)		{0}", minMajor * 1000);
-		statsWriter.WriteLine ("Max major (ms)		{0}", maxMajor * 1000);
-		statsWriter.WriteLine ("Avg major (ms)		{0}", avgMajor * 1000);
+		runStats.totalPause = totalPause * 1000;
+                runStats.numMinorStops = numMinor;
+                runStats.minMinor = minMinor * 1000;
+                runStats.maxMinor = maxMinor * 1000;
+                runStats.avgMinor = avgMinor * 1000;
+                runStats.numMajorStops = numMajor;
+                runStats.minMajor = minMinor * 1000;
+                runStats.maxMajor = maxMajor * 1000;
+                runStats.avgMajor = avgMajor * 1000;
 
 		/* Memory Usage */
 		float memoryUsageStat = 0.0f;
 		for (int i = 1; i < memoryUsage.Count - 1; i++)
 			memoryUsageStat += (memoryUsage [i].x - memoryUsage [i - 1].x) * (memoryUsage [i].y + memoryUsage [i - 1].y) / 2;
-		memoryUsageStat *= referenceTime / totalTime;
+		runStats.memoryUsage = memoryUsageStat * referenceTime / runStats.totalTime;
+
+		return runStats;
+	}
+
+	public static void OutputStat (string prefix, RunStats runStats, StreamWriter statsWriter) {
+		statsWriter.WriteLine ("\n" + prefix);
+		statsWriter.WriteLine ("Time (s)		{0}", runStats.totalTime);
+		statsWriter.WriteLine ("Minor			{0}", runStats.numMinor);
+		statsWriter.WriteLine ("Major			{0}", runStats.numMajor);
+		statsWriter.WriteLine ("Minor While Major	{0}", runStats.numMinorWhileMajor);
+
 		statsWriter.WriteLine ();
-		statsWriter.WriteLine ("Memory Usage (MBs)	{0}", memoryUsageStat);
+		statsWriter.WriteLine ("Total pause time (ms)	{0}", runStats.totalPause);
+		statsWriter.WriteLine ("Minor stops		{0}", runStats.numMinor);
+		statsWriter.WriteLine ("Min minor (ms)		{0}", runStats.minMinor);
+		statsWriter.WriteLine ("Max minor (ms)		{0}", runStats.maxMinor);
+		statsWriter.WriteLine ("Avg minor (ms)		{0}", runStats.avgMinor);
+		statsWriter.WriteLine ("Major stops		{0}", runStats.numMajor);
+		statsWriter.WriteLine ("Min major (ms)		{0}", runStats.minMajor);
+		statsWriter.WriteLine ("Max major (ms)		{0}", runStats.maxMajor);
+		statsWriter.WriteLine ("Avg major (ms)		{0}", runStats.avgMajor);
+
+		statsWriter.WriteLine ();
+		statsWriter.WriteLine ("Memory Usage (MBs)	{0}", runStats.memoryUsage);
 	}
 
 	public static void RunMono (string mono, string[] args, string workingDirectory, bool concurrent) {
@@ -254,7 +323,7 @@ public class Program {
 		}
 	}
 
-	public static void ParseBinProtOutput (bool concurrent) {
+	public static void ParseBinProtOutput () {
 		Process p = new Process ();
 		p.StartInfo.UseShellExecute = false;
 		p.StartInfo.FileName = binprotExec; 
@@ -268,8 +337,7 @@ public class Program {
 		p.WaitForExit ();
 
 		ParseNurseryCollections (stdout);
-		if (concurrent)
-			ParseConcurrentCollections (stdout);
+		ParseConcurrentCollections (stdout);
 		ParseMajorCollections (stdout);
 		ParseStopIntervals (stdout);
 	}
@@ -315,5 +383,43 @@ public class Program {
 		using (FileStream stream = new FileStream (name, FileMode.Create)) {
 			SvgExporter.Export (plotModel, stream, 1920, 1080, true);
 		}
+	}
+}
+
+public class RunStats : IComparable<RunStats> {
+	public float totalTime;
+	public float minMinor;
+	public float maxMinor;
+	public float minMajor;
+	public float maxMajor;
+	public float avgMinor;
+	public float avgMajor;
+	public float totalPause;
+	public int numMinor, numMinorStops;
+	public int numMajor, numMajorStops;
+	public int numMinorWhileMajor;
+	public float memoryUsage;
+
+	public RunStats ()
+	{
+		totalTime = 0.0f;
+		minMinor = float.MaxValue;
+		maxMinor = float.MinValue;
+		minMajor = float.MaxValue;
+		maxMajor = float.MinValue;
+		avgMinor = 0.0f;
+		avgMajor = 0.0f;
+		totalPause = 0.0f;
+		numMinor = 0;
+		numMinorStops = 0;
+		numMajor = 0;
+		numMajorStops = 0;
+		numMinorWhileMajor = 0;
+		memoryUsage = 0.0f;
+	}
+
+	public int CompareTo (RunStats other)
+	{
+		return totalTime.CompareTo (other.totalTime);
 	}
 }
